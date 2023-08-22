1fcdf47c33928ff39efdb4aa181aeba3fdf02a49
jackspell <m.metodij@outlook.com>
2023-08-22 17:35:29 +0200

    Added database.php as an intermediate between php and interacting with the database

---
+++ Pages/database.php
@@ -0,0 +1,37 @@
+<?php
+    class Database{
+        private $db_server;
+        private $db_user;
+        private $db_password;
+        private $db_name;
+        private $conn;
+
+        public function __construct() {
+            $this->db_server = "localhost";
+            $this->db_user = "root";
+            $this->db_password = "toor";
+            $this->db_name = "appointmentappdb";
+
+            try{
+                $this->conn = new mysqli($this->db_server, $this->db_user, $this->db_password, $this->db_name);
+            }
+            catch(mysqli_sql_exception){
+                echo "Database connection error!";
+                exit();
+            }
+        }
+
+        public function executeQuery($qstring){
+            try{
+                $result = $this->conn->query($qstring);
+                $this->conn -> close();
+                return $result;
+            }
+            catch(mysqli_sql_exception){
+                echo "Database connection error!";
+                $this->conn -> close();
+                exit();
+            }
+        }
+    }
+?>
--- Pages/index.php
+++ Pages/index.php
@@ -7,8 +7,12 @@
     <body>
         The content of the document......
         <?php
-            echo "And a php example " . "test";
+            echo "And a php example " . "test<br>";
+            include("database.php");
+            $obj = new Database();
+            $status = $obj->executeQuery("SELECT * FROM Appointments")->fetch_assoc()["appointment_id"] . "<br>";
+            echo $status;
         ?>
     </body>
 
-</html>
+</html> 
